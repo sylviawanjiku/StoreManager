acf7073fb3553ccbfe19604bc53ba5d21aa9bfb5
sylviawanjiku <sylviakatherine22@gmail.com>
2018-11-10 14:49:27 +0300

    [feature #161858093]Create a user successfully

--- UI/Assets/js/script.js
+++ UI/Assets/js/script.js
@@ -33,74 +33,77 @@ let login = document.getElementById('login_form')
        }
       }
 // Login.......................................................................
-// var signup = document.getElementById('register')
-// if (signup){
-//   signup.addEventListener('submit', Signup);
+var signup = document.getElementById('register')
+if (signup){
+signup.addEventListener('submit', Signup);
 
-//   function Signup(e){
-//     e.preventDefault();
+function Signup(e){
+  e.preventDefault();
 
-//     let token = localStorage.getItem('token')
+  let token = localStorage.getItem('token')
 
-//     const username = document.getElementById('username').value;
-//     const firstname = document.getElementById('fname').value;
-//     const lastname = document.getElementById('lname').value;
-//     const password = document.getElementById('password').value;
-//     const email = document.getElementById('email').value;
-//     const is_admin = document.getElementById('admin').value;
+  let username = document.getElementById('username').value;   
+  let first_name = document.getElementById('first_name').value;
+  let last_name = document.getElementById('last_name').value;
+  let password = document.getElementById('password').value;
+  let email = document.getElementById('email').value; 
+  let is_admin = document.getElementById('admin').value;
 
-//     const signup_data= {
-//       "username": username,
-//       "firstname": firstname,
-//       "lastname": lastname,
-//       "password": password,
-//       "email": email,
-//       "is_admin": is_admin
-//     }
+  // const signup_data= {
+  //   "username": username,
+  //   "email": email,
+  //   "firstname": firstname,
+  //   "lastname": lastname,
+  //   "password": password,     
+  //   "is_admin": is_admin
+  // }
 
-//     fetch('http://127.0.0.1:5000/api/v2/signup', {
-//       method: 'POST',
-//       headers: {
-//         'Accept': 'application/json, text/plain, */*',
-//         'Content-type': 'application/json', 
-//         'Access-Control-Allow-Origin':'*',
-//         'Access-Control-Request-Method': '*',
-//         "Authorization": token
-//       },
-//       body: JSON.stringify({email:email, password:password})
-//     })
-//     .then((res)=> res.json())
-//     .then((data)=>{
-//       let message =document.getElementById('message')
-//       if(data.message == "enter valid email"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "username must be a string"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "Firstname must be a string"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "Lastname must be a string"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "Password should start with a capital letter and include a number"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "User already exists"){
-//         message.style.color = 'red';
-//         error_reg.innerHTML= data.message;
-//       }
-//       if(data.message == "user created successfully"){
-//         message.style.color = 'green';
-//         error_reg.innerHTML= data.message;
-//       } 
-//     })
-//     .catch((err) => console.log(err))
-//   }  
-// } 
+  fetch('http://127.0.0.1:5000/api/v2/signup', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-type': 'application/json', 
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Request-Method': '*',
+      "Authorization": 'Bearer ' + token
+    },
+    body: JSON.stringify({username:username,first_name:first_name,last_name:last_name,
+                          password:password, email:email,is_admin:is_admin})
+  })
+  .then((res)=> res.json())
+  // .then((data)=>console.log(data))
+  .then((data)=>{
+    console.log(data)
+    // let message =document.getElementById('message')
+    if(data.message == "enter valid email"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;
+    }
+    if(data.message == "username must be a string"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;
+    }
+    if(data.message == "Firstname must be a string"){
+      document.getElementById("message").style.color = 'red';             
+      ocument.getElementById('message').innerHTML = data.message;
+    }
+    if(data.message == "Lastname must be a string"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;;
+    }
+    if(data.message == "Password should start with a capital letter and include a number"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;
+    }
+    if(data.message == "User already exists"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;
+    }
+    if(data.message == "user created successfully"){  
+                          
+      window.location ='http://127.0.0.1:5500/UI/html/employee.html';
+    }
+  })
+  .catch((err) => console.log(err))
+}  
+} 
