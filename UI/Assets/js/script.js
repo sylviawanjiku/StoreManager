1c31a14cb1efac4cbaa32d8fbf77437328369327
sylviawanjiku <sylviakatherine22@gmail.com>
2018-11-12 15:29:47 +0300

    [feature #161878665]Can get all users and search for aspecific user and product

--- UI/Assets/js/script.js
+++ UI/Assets/js/script.js
@@ -23,9 +23,10 @@ let login = document.getElementById('login_form')
               if (data.message == "User not found"){  
                 //  document.getElementById("message").style.color = 'red';             
                 //  document.getElementById('message').innerHTML = data.message
+                alert('User not found')
               }
               if (data.message =="Successfully logged In"){  
-                          
+                alert('Successfully logged In')
                 window.location ='http://127.0.0.1:5500/UI/html/dashboard.html';
               }
               window.localStorage.setItem('token',data.token);
@@ -109,6 +110,70 @@ function Signup(e){
 } 
 
 // signup------------------------------------------------------------------------------------------
+function userFilter() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("userInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("users-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+document.getElementById('viewAllUsers').addEventListener('click', viewUsers);
+    
+    function viewUsers(){
+
+      let userList = document.getElementById('users-body')
+      let token = localStorage.getItem('token')
+
+      fetch('http://127.0.0.1:5000/api/v2/users',{
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-type': 'application/json', 
+          'Access-Control-Allow-Origin':'*',
+          'Access-Control-Request-Method': '*',
+          "Authorization": 'Bearer ' + token
+        }
+      })
+     .then((res)=> res.json())
+     .then(data => {
+      if (data.message =="The users list is empty"){
+      document.getElementById("message").style.color = 'red';             
+      document.getElementById('message').innerHTML = data.message;
+      }
+      if(data.message =="Users retrieved successfully"){
+          data.users.forEach((user,index) => {
+              userList.innerHTML += `<tr>
+                      
+                      <td>${user.username}</td>                      
+                      <td> ${user.first_name}</td>
+                      <td> ${user.last_name}</td>
+                      <td> ${user.password}</td>                     
+                      <td> ${user.email}</td>                      
+                      <td> ${user.is_admin}</td>
+                      <td>
+                          <button>Edit Role</button>
+                      </td> 
+                    </tr>`;       
+            
+          });
+          
+        }
+    })
+    .catch((err) => console.log(err))
+   }
+// allusers----------------------------------------------------------------------------------------------------------------------------------------------
+
 var createproduct = document.getElementById('addproduct')
     if (createproduct){
     createproduct.addEventListener('submit', CreateProduct);
@@ -181,51 +246,3 @@ var createproduct = document.getElementById('addproduct')
 }  
 }
 // addproduct----------------------------------------------------------------------------------------------
-document.getElementById('viewProducts').addEventListener('click', viewProducts);
-    
-    function viewProducts(){
-
-      let productList = document.getElementById('prod-body')
-      let token = localStorage.getItem('token')
-
-      fetch('http://127.0.0.1:5000/api/v2/products',{
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-type': 'application/json', 
-          'Access-Control-Allow-Origin':'*',
-          'Access-Control-Request-Method': '*',
-          "Authorization": 'Bearer ' + token
-        }
-      })
-     .then((res)=> res.json())
-     .then(data => {
-      if (data.message =="The product list is empty"){
-      document.getElementById("message").style.color = 'red';             
-      document.getElementById('message').innerHTML = data.message;
-      }
-      if(data.message =="Products retrieved successfully"){
-          data.products.forEach((product,index) => {
-              productList.innerHTML += `<tr>
-                      
-                      <td>${product.product_name}</td>                      
-                      <td> ${product.brand}</td>
-                      <td> ${product.quantity}</td>
-                      <td> ${product.price}</td>                     
-                      <td> ${product.avail_stock}</td>                      
-                      <td> ${product.min_stock}</td>
-                      <td> ${product.uom}</td>
-                      <td> ${product.category}</td>
-                      <td><button>view</button>
-                          <button>Edit</button>
-                          <button>Delete</button>
-                      </td> 
-                    </tr>`;       
-            
-          });
-          
-        }
-    })
-    .catch((err) => console.log(err))
-   }
-  // get all products--------------------------------------------------------
